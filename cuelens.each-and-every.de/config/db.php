97f1622dd2e7a89058065752df11b859f28ef129
Stefan <s.berger.bmio@gmail.com>
2026-05-25 22:16:04 +0200

    Datenschutzerklärung, Studieninformation, Flyer, Webformular

---
+++ cuelens.each-and-every.de/config/db.php
@@ -0,0 +1,8 @@
+<?php
+// Datei wird direkt auf dem Server bearbeitet, um Passwort zu schützen.
+return [
+    'host' => 'localhost',
+    'dbname' => 'deine_datenbank',
+    'user' => 'dein_benutzer',
+    'pass' => 'dein_passwort',
+];
