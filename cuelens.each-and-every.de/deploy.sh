c469b6a13f3436878cddc05530ca3ef7b332d2b6
Stefan <s.berger.bmio@gmail.com>
2026-06-21 20:48:27 +0200

    inital tests

--- cuelens.each-and-every.de/deploy.sh
+++ cuelens.each-and-every.de/deploy.sh
@@ -1,2 +1,6 @@
-cp *.html *.js *.css *.php *.pdf /var/www/html/cuelens
-cp -r lib /var/www/html/cuelens
+if vendor/bin/phpunit tests; then
+  cp *.html *.js *.css *.php *.pdf /var/www/html/cuelens
+  cp -r lib /var/www/html/cuelens
+else
+  echo 'Not deploying due to test failures'
+fi
--- cuelens.each-and-every.de/ds.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("Location: ./datenschutzerklärung.pdf");
-exit;
