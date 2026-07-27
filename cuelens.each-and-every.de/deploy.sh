355cb33749910d7722b9221fb43c63edd3310e00
Stefan Berger <mail@stefan-berger.me>
2026-07-27 20:16:50 +0200

    deploy script update

--- cuelens.each-and-every.de/deploy.sh
+++ cuelens.each-and-every.de/deploy.sh
@@ -1,6 +1,6 @@
 if vendor/bin/phpunit tests; then
   cp *.html *.js *.css *.php *.pdf /var/www/html/cuelens
-  cp -r lib /var/www/html/cuelens
+  cp -r lib img /var/www/html/cuelens
 else
   echo 'Not deploying due to test failures'
 fi
